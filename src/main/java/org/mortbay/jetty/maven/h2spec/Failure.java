fa5d356ca442fa116beaaaab793641735bfa9bd7
Olivier Lamy <olamy@apache.org>
2024-06-27 11:28:10 +1000

    spotless apply
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/org/mortbay/jetty/maven/h2spec/Failure.java
+++ src/main/java/org/mortbay/jetty/maven/h2spec/Failure.java
@@ -7,7 +7,8 @@ public class Failure {
     private final String expected;
     private final boolean ignored;
 
-    public Failure(final String name, final String specId, final String actual, final String expected, final boolean ignored) {
+    public Failure(
+            final String name, final String specId, final String actual, final String expected, final boolean ignored) {
         this.name = name;
         this.specId = specId;
         this.actual = actual;
@@ -17,7 +18,8 @@ public class Failure {
 
     @Override
     public String toString() {
-        return "[" + H2SpecTestSuite.getSpecIdentifier(specId, name) + "]" + " failed\n Expected: " + expected + "\n Actual: " + actual;
+        return "[" + H2SpecTestSuite.getSpecIdentifier(specId, name) + "]" + " failed\n Expected: " + expected
+                + "\n Actual: " + actual;
     }
 
     public boolean isIgnored() {
--- src/test/java/org/mortbay/jetty/maven/h2spec/Http2SpecMojoTest.java
+++ src/test/java/org/mortbay/jetty/maven/h2spec/Http2SpecMojoTest.java
@@ -1,9 +1,6 @@
 package org.mortbay.jetty.maven.h2spec;
 
-import org.codehaus.plexus.util.xml.Xpp3Dom;
-import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
-import org.junit.Test;
-import org.mortbay.jetty.maven.h2spec.Http2SpecMojo;
+import static org.junit.Assert.assertEquals;
 
 import java.io.Reader;
 import java.nio.file.Files;
@@ -11,45 +8,49 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
-public class Http2SpecMojoTest
-{
+public class Http2SpecMojoTest {
 
     @Test
-    public void failedTestReallyMarkedAsSkipped() throws Exception
-    {
+    public void failedTestReallyMarkedAsSkipped() throws Exception {
         Http2SpecMojo mojo = new Http2SpecMojo();
-        mojo.setExcludeSpecs( Arrays.asList( new String[]{ "3.5 - Sends invalid connection preface"} ));
-        Path junitOrigin = Paths.get( "src/test/resources/TEST-h2spec.xml" );
-        Path junit = Paths.get( "target/TEST-h2spec.xml" );
-        Files.copy( junitOrigin, junit, StandardCopyOption.REPLACE_EXISTING );
-        mojo.markedFailedTestAsSkipped( junit );
+        mojo.setExcludeSpecs(Arrays.asList(new String[] {"3.5 - Sends invalid connection preface"}));
+        Path junitOrigin = Paths.get("src/test/resources/TEST-h2spec.xml");
+        Path junit = Paths.get("target/TEST-h2spec.xml");
+        Files.copy(junitOrigin, junit, StandardCopyOption.REPLACE_EXISTING);
+        mojo.markedFailedTestAsSkipped(junit);
 
-        try(Reader reader = Files.newBufferedReader( junit ))
-        {
-            Xpp3Dom dom = Xpp3DomBuilder.build( reader);
-            assertEquals(1, Arrays.stream( dom.getChildren()).
-                filter( testsuite ->  testsuite.getAttribute( "id" ).equals( "3.5" ) &&
-                                    testsuite.getAttribute( "skipped" ).equals( "1" )).
-                count());
-            assertEquals(2, Arrays.stream( dom.getChildren()).
-                filter( testsuite ->  testsuite.getAttribute( "id" ).equals( "3.5" ) &&
-                    testsuite.getAttribute( "errors" ).equals( "0" )).
-                count());
+        try (Reader reader = Files.newBufferedReader(junit)) {
+            Xpp3Dom dom = Xpp3DomBuilder.build(reader);
+            assertEquals(
+                    1,
+                    Arrays.stream(dom.getChildren())
+                            .filter(testsuite -> testsuite.getAttribute("id").equals("3.5")
+                                    && testsuite.getAttribute("skipped").equals("1"))
+                            .count());
+            assertEquals(
+                    2,
+                    Arrays.stream(dom.getChildren())
+                            .filter(testsuite -> testsuite.getAttribute("id").equals("3.5")
+                                    && testsuite.getAttribute("errors").equals("0"))
+                            .count());
 
-            assertEquals(1, Arrays.stream( dom.getChildren()).
-                filter( testsuite ->  testsuite.getAttribute( "skipped" ).equals( "1" )).
-                count());
+            assertEquals(
+                    1,
+                    Arrays.stream(dom.getChildren())
+                            .filter(testsuite ->
+                                    testsuite.getAttribute("skipped").equals("1"))
+                            .count());
 
-            assertEquals(0, Arrays.stream( dom.getChildren()).
-                filter( testsuite ->  !testsuite.getAttribute( "errors" ).equals( "0" )).
-                count());
-
+            assertEquals(
+                    0,
+                    Arrays.stream(dom.getChildren())
+                            .filter(testsuite ->
+                                    !testsuite.getAttribute("errors").equals("0"))
+                            .count());
         }
-
-
     }
-
 }
